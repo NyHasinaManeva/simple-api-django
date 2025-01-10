7b1847fe207b2059ae4418ca4619e19c1561a572
Maneva <randriamalalamaneva@gmail.com>
2025-01-10 16:07:58 +0300

    ajout des fonctions pour creer, supprimer et modifier une note

--- notes/urls.py
+++ notes/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('notes/',views.note,name='notes')
+    path('notes/',views.note,name='notes'),
+    path('new_note',views.new_note,name='new note'),
+    path('notes/<slug:slug>',views.detail_note,name='detail_note')
 ]
--- notes/views.py
+++ notes/views.py
@@ -14,6 +14,39 @@ def note(req):
             return Response(serializers.data)
         except Note.DoesNotExist:
             return Response("N'existe pas",status=status.HTTP_404_NOT_FOUND)
+        
+@api_view(["POST"])
+def new_note(req):
+    if req.method=="POST":
+        note = req.data
+        serializer = NoteSerializer(data = note)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+@api_view(["GET","PUT","DELETE"])
+def detail_note(req,slug):
+    try:
+        note=Note.objects.get(slug=slug)
+    except Note.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if req.method=="GET":
+        serializer = NoteSerializer(note)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    elif req.method=="DELETE":
+        note.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    elif req.method=="PUT":
+        serializer = NoteSerializer(note,data=req.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_404_NOT_FOUND)
+
+
+
     
 
 
